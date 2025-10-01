b2da3a9405230107786e9b02c283caca64b98c73
AlekseyF7 <fadeevaleksej32@gmail.com>
2025-10-01 13:02:35 +0500

    2 задача про Стаса

---
+++ Practica/task2.py
@@ -0,0 +1,17 @@
+import random
+
+def check_winners(scores, student_score):
+    result = ''
+    top_three = sorted(scores, reverse=True)[:3]
+    if student_score in top_three:
+        result = 'Вы в тройке победителей!'
+    else:
+        result = 'Вы не попали в тройку победителей.'
+    return result
+
+scores = random.sample(range(1,101), 10)
+student_score = int(input('Введите баллы Стаса: '))
+if student_score not in scores:
+    scores.append(student_score)
+result = check_winners(scores, student_score)
+print(result)
