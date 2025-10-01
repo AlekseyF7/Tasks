f8a51dc1fdc343a57ccd63de1a36a9912d199973
AlekseyF7 <fadeevaleksej32@gmail.com>
2025-10-01 13:04:06 +0500

    задача про Стаса

--- Practica/task2.py
+++ Practica/task2.py
@@ -14,4 +14,5 @@ student_score = int(input('Введите баллы Стаса: '))
 if student_score not in scores:
     scores.append(student_score)
 result = check_winners(scores, student_score)
+
 print(result)
